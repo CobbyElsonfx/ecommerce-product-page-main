0a6da8e1e42b258d9c5da53db398a23f85633b45
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-03-21 02:37:26 +0000

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+          "./*.html",
+          "./**/*.js",
+
+            ],
   theme: {
-    extend: {},
+    screens: {
+      sm:"480px",
+      md:"768px",
+      lg:"976px",
+      xl:"1440px"
+
+      },
+
+
+    extend: {
+      colors:{
+        veryDarkBlue: "hsl(220, 13%, 13%)",
+        darkGrayishBlue: "hsl(219, 9%, 45%)",
+        grayishBlue: "hsl(220, 14%, 75%)",
+        lightGrayishBlue: "hsl(223, 64%, 98%)",
+        white: "hsl(0, 0%, 100%)"
+        // black (with 75% opacity for lightbox background): hsl(0, 0%, 0%)
+      }
+    },
   },
   plugins: [],
 }
