28cd366802c60ef5daff0c34df70b3251dae6d57
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-05-17 03:37:34 +0000

    carousel mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
           "./*.html",
           "./**/*.js",
+          "./node_modules/flowbite/**/*.js"
 
             ],
   theme: {
@@ -29,5 +30,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+        require('flowbite/plugin')
+    ],
 }
